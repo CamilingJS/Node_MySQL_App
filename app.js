f4f6460b13561642fe81e14850a8710997775ebb
CamilingJS <jonathancamiling@gmail.com>
2024-11-01 16:27:22 -0700

    installed mysql package and established connection to mysql db

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// const { faker } = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+let q = 'SELECT CURDATE()';
+connection.query(q, function(error, results, fields){
+    if(error) throw error;
+    console.log(results[0])
+});
+
+connection.end();
+
+  
+//   console.log(connection)
+
+// console.log(results[0].time);
+// console.log(results[0].date.toString());
+// console.log(results[0].now.toString());
