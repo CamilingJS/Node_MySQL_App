82451c1e113b3053f1a4c5cb6134ffe452d7d1a6
CamilingJS <jonathancamiling@gmail.com>
2024-11-03 12:25:04 -0800

    bulk insert of 500 users generated by Faker into our SQL Database

--- app.js
+++ app.js
@@ -7,19 +7,40 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-  });
+});
 
-let q = 'SELECT * FROM test';
-connection.query(q, function(error, results, fields){
-    if(error) throw error;
-    console.log(results[0])
+// let q = "SELECT COUNT(*) AS users_count FROM users";
+// let q = 'INSERT INTO users (email) VALUES ("autumnf@gmail.com")'; 
+// connection.query(q, function(error, results, fields){
+//     if(error) throw error;
+//     console.log(results)
+// });
+
+// connection.end();
+
+// let person = {email: faker.internet.email()};
+// connection.query('INSERT INTO users SET ?', person, function(error, results){
+//   if(error) throw error;
+//   console.log(results);
+// });
+
+// connection.end();
+
+// Inserting lots of data:
+let data = [];
+
+for (let i=0; i<500; i++){
+  data.push([
+    faker.internet.email(),
+    faker.date.past()
+  ]); 
+}
+
+let q = 'INSERT INTO users (email, created_at) VALUES ?';
+
+connection.query(q, [data], function(error, results){
+  if(error) throw error;
+  console.log(results);
 });
 
 connection.end();
-
-  
-// console.log(connection)
-
-// console.log(results[0].time);
-// console.log(results[0].date.toString());
-// console.log(results[0].now.toString());
