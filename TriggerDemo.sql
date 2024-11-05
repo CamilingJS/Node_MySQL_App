1f8eeb59fac893d3030c40fe166d21deef4ef821
CamilingJS <jonathancamiling@gmail.com>
2024-11-05 15:41:12 -0800

    added trigger demo file

---
+++ TriggerDemo.sql
@@ -0,0 +1,65 @@
+-- Boilerplate starter code
+DELIMITER $$
+
+CREATE TRIGGER trigger_name
+     trigger_time trigger_event ON table_name FOR EACH ROW
+     BEGIN
+     END;
+$$
+
+DELIMITER ;
+
+-- Restrict age to 18 and above
+DELIMITER $$
+
+CREATE TRIGGER must_be_adult
+     BEFORE INSERT ON users FOR EACH ROW
+     BEGIN
+          IF NEW.age < 18
+          THEN
+              SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Must be an adult!';
+          END IF;
+     END;
+$$
+
+DELIMITER ;
+
+-- Restriction followers can't follow themselves
+DELIMITER $$
+
+CREATE TRIGGER example_cannot_follow_self
+     BEFORE INSERT ON follows FOR EACH ROW
+     BEGIN
+          IF NEW.follower_id = NEW.following_id
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot follow yourself, silly';
+          END IF;
+     END;
+$$
+
+DELIMITER ;
+
+-- Track unfollowing events
+DELIMITER $$
+
+CREATE TRIGGER create_unfollow
+    AFTER DELETE ON follows FOR EACH ROW 
+BEGIN
+    INSERT INTO unfollows
+    SET follower_id = OLD.follower_id,
+        followee_id = OLD.followee_id;
+END$$
+
+DELIMITER ;
+
+-- Managing Triggers
+SHOW TRIGGERS; 
+
+DROP TRIGGER trigger_name;
+
+
+
+
+
